d40e6e67a620aed0849bd1f5e0ea2af0ddb9d6a4
Mar Anjorie Navarez <mar.a.b.navarez@accenture.com>
2021-01-14 02:09:23 +0800

    JS - Arrays

---
+++ Week 3/Array Average with Undefined Elements.js
@@ -0,0 +1,11 @@
+var grades =[];
+var sum = 0;
+
+if(grades.length > 0){
+	for(index =0; index < grades.length; index++){
+		sum += grades.length[index];
+	}
+}
+else {
+	document.write("Empty Array");
+}
